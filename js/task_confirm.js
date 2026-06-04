0bdca4af45cf7150d76589243d859f96417dbfc1
2501551-abcc <2501551@s.asojuku.ac.jp>
2026-06-04 12:29:30 +0900

    通知関連のコミット
    通知から遷移し、遷移した先で完了を押すとタスクリストに「完了」のdiv内で表示されるようになりました

--- js/task_confirm.js
+++ js/task_confirm.js
@@ -1,18 +1,21 @@
-window.addEventListener('load', () => {
-    let task_content = localStorage.getItem("task_content");
-    let task_display = document.getElementById("now_task");
+let task_content = localStorage.getItem("task_content") || "未選択";
+let task_display = document.getElementById("now_task");
+
+if (task_display) {
     task_display.textContent = task_content;
-});
+} else {
+    console.error("now_taskというIDのタグが見つかりませんし！");
+}
 
 
 //完了したタスクの内容を読み取り、タスク内容_task = trueとかで格納する
 function task_complete(){
-    complete_task = document.getElementById("now.task").value;
-    if(complete_task = "朝ごはん"){
+    complete_task = document.getElementById("now_task").textContent;
+    if(complete_task == "朝ごはん"){
         localStorage.setItem("breakfast_task", "true");
-    }else if(complete_task = "夜ごはん"){
+    }else if(complete_task == "夜ごはん"){
         localStorage.setItem("dinner_task", "true");
-    }else if(complete_task = "お風呂"){
+    }else if(complete_task == "お風呂"){
         localStorage.setItem("bath_task", "true");
     }
     window.location.href = "task_list.html"; 
